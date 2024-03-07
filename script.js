e7ec1c45e440c8c2774b4f5d18695d8abdc9d525
kashishkoli <134147373+kashishkoli@users.noreply.github.com>
2024-03-07 22:50:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
+    if (num <= 0) {
+        return "Invalid input";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let fibSeries = [0, 1];
+        for (let i = 2; i < num; i++) {
+            fibSeries.push(fibSeries[i - 1] + fibSeries[i - 2]);
+        }
+        return fibSeries[num - 1];
+    }
 }
 
 module.exports = fibonacci;
